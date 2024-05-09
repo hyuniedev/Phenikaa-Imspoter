629d156aa7ada58923845d9fcf32a9ee79d42896
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-05-10 04:38:20 +0700

    add read data

--- Phenikaa_Imspoter/Assets/HyuNie/Scripts/EObject.cs
+++ Phenikaa_Imspoter/Assets/HyuNie/Scripts/EObject.cs
@@ -1,5 +1,6 @@
 public enum EObject
 {
+    Bot,
     Human,
     Grost
 }
--- Phenikaa_Imspoter/Assets/HyuNie/Scripts/ReadData/ReadData.cs
+++ Phenikaa_Imspoter/Assets/HyuNie/Scripts/ReadData/ReadData.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ReadData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static ReadData instance;
+    public static ReadData Instance
     {
-        
+        get
+        {
+            return instance;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private static int lineIndex;
+    private string PATH_DATA = Application.dataPath + "/Data/data.csv";
+    private string[] datas;
+    private void Awake()
     {
-        
+        instance = this;
+    }
+    private void Start()
+    {
+        lineIndex = 1;
+        Debug.Log(PATH_DATA);
+        if (!File.Exists(PATH_DATA))
+        {
+            File.WriteAllText(PATH_DATA, "");
+        }
+        datas = File.ReadAllLines(PATH_DATA);
+    }
+    public void getData(InforPeople inforPeople)
+    {
+        string line = datas[lineIndex];
+        if (line.Length > 0)
+        {
+            string[] data = line.Split(",");
+            inforPeople.ID = long.Parse(data[0]);
+            inforPeople.eObject = data[1].Equals("TRUE") ? EObject.Bot : randomEObject();
+            inforPeople.Name = data[2];
+        }
+        else
+        {
+            inforPeople.Name = "Het Data";
+            inforPeople.ID = 123456789;
+            inforPeople.eObject = EObject.Grost;
+        }
+        lineIndex++;
+    }
+    private EObject randomEObject()
+    {
+        return Random.Range(0, 2) == 0 ? EObject.Human : EObject.Grost;
     }
 }
