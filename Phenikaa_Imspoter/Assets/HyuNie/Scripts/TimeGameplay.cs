e7b728018faa9b0b74a2941a089b1d4c431ce709
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-05-10 07:40:58 +0700

    + Complete End Day

--- Phenikaa_Imspoter/Assets/HyuNie/Scripts/TimeGameplay.cs
+++ Phenikaa_Imspoter/Assets/HyuNie/Scripts/TimeGameplay.cs
@@ -5,15 +5,29 @@ using UnityEngine.UI;
 public class TimeGameplay : MonoBehaviour
 {
     [SerializeField] Text txtTime;
-    private float speedTime = 5;
+    private float speedTime = 2;
     private float h, m;
+    private float HourEnd, MinuteEnd;
     private void Start()
     {
         h = 9;
         m = 0;
+        HourEnd = 12;
+        MinuteEnd = 30;
     }
     void Update()
     {
+        if (h >= HourEnd && m >= MinuteEnd)
+        {
+            GameController.Instance.Begin_Or_End_Day();
+            GameController.Instance.isCompleteDay = true;
+            return;
+
+        }
+        else
+        {
+            GameController.Instance.isCompleteDay = false;
+        }
         m += Time.deltaTime * speedTime;
         if (m >= 60)
         {
@@ -23,5 +37,11 @@ public class TimeGameplay : MonoBehaviour
         String gio = ((int)h).ToString().Length == 1 ? "0" + (int)h : ((int)h).ToString();
         String phut = ((int)m).ToString().Length == 1 ? "0" + (int)m : ((int)m).ToString();
         txtTime.text = gio + " : " + phut;
+    }
+    public void resetTime()
+    {
+        GameController.Instance.isCompleteDay = false;
+        h = 9;
+        m = 0;
     }
 }
