bb70212b9826c63a69f4881945cdd6181198dcab
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-05-20 08:00:57 +0700

    + scale item go to yellow area

--- Phenikaa_Imspoter/Assets/HyuNie/Scripts/CheckMouse.cs
+++ Phenikaa_Imspoter/Assets/HyuNie/Scripts/CheckMouse.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 public class CheckMouse : MonoBehaviour
 {
     private GameObject target;
+    private Vector2 posMouse;
     private void Update()
     {
+        posMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         checkMouseButton();
+        checkPointsMouse();
     }
+
+    private void checkPointsMouse()
+    {
+        if (target == null) return;
+        Vector2 posYellow = GameController.Instance.getPointMouse("Yellow");
+        if (posMouse.x > posYellow.x && posMouse.y < posYellow.y)
+        {
+            target.gameObject.transform.localScale = new Vector2(2, 2);
+        }
+        else
+        {
+            target.gameObject.transform.localScale = new Vector2(1, 1);
+        }
+    }
+
     private void checkMouseButton()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit2D = Physics2D.Raycast(ray, Vector2.zero, Mathf.Infinity, LayerMask.GetMask("Nimotsu"));
+            RaycastHit2D hit2D = Physics2D.Raycast(posMouse, Vector2.zero, Mathf.Infinity, LayerMask.GetMask("Nimotsu"));
             if (hit2D.collider != null)
             {
                 target = hit2D.collider.gameObject;
--- Phenikaa_Imspoter/Assets/HyuNie/Scripts/GameController.cs
+++ Phenikaa_Imspoter/Assets/HyuNie/Scripts/GameController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
@@ -12,6 +14,8 @@ public class GameController : MonoBehaviour
     public bool isCompleteDay { get; set; } = false;
     private Aite aite;
     [SerializeField] private Transform[] transforms;
+    [SerializeField] private Transform[] pointsMouse;
+    private Dictionary<string, Vector2> dicPointsMouse = new Dictionary<string, Vector2>();
     private void Awake()
     {
         aite = FindObjectOfType<Aite>();
@@ -20,10 +24,22 @@ public class GameController : MonoBehaviour
     private void Start()
     {
         isCompleteDay = false;
+        createDictionaryPointMouse();
     }
+
+    private void createDictionaryPointMouse()
+    {
+        dicPointsMouse.Add("Yellow", pointsMouse[0].position);
+    }
+
     public Transform getTransform(int index)
     {
         return transforms[index];
+    }
+    public Vector2 getPointMouse(string area)
+    {
+        // Yellow = 0
+        return dicPointsMouse[area];
     }
     public void Begin_Or_End_Day()
     {
